42adde65ec3ef8f04fb0261056e9150a361b7e71
Gnashal <yousifceballos@gmail.com>
2025-09-06 21:12:05 +0800

    add: account proto, account grpc server initialized

--- services/account/main.go
+++ services/account/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"handworks-services-account/server"
+	"handworks-services-account/service"
+	"handworks/common/grpc/account"
+	"handworks/common/utils"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	// main here
+	godotenv.Load()
+
+	logger, err := utils.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+
+	accService := service.AccountService{
+		L:                                 logger,
+		UnimplementedAccountServiceServer: account.UnimplementedAccountServiceServer{},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		if err := server.StartGrpcServer(&accService, logger); err != nil {
+			logger.Fatal("Initialization of Account GRPC Server Failed: %v", err)
+		}
+	}()
+
+	wg.Wait()
 }
--- services/account/service/service.go
+++ services/account/service/service.go
@@ -1,3 +1,18 @@
 package service
 
-// type AccountService
+import (
+	"context"
+	"handworks/common/grpc/account"
+	"handworks/common/utils"
+)
+
+type AccountService struct {
+	L    *utils.Logger
+	Grpc account.AccountServiceServer
+	account.UnimplementedAccountServiceServer
+}
+
+func (a *AccountService) SignUp(c context.Context, in *account.SignUpRequest) (*account.SignUpResponse, error) {
+
+	return nil, nil
+}
