4f609fe7c2955bed479192d481977d11489982f9
Gnashal <yousifceballos@gmail.com>
2025-09-18 02:03:44 +0800

    fix: type mismatch

--- gateway/internal/middleware/auth.go
+++ gateway/internal/middleware/auth.go
@@ -13,13 +13,15 @@ type ContextString string
 
 const ClerkClaimsKey ContextString = "clerk-claims"
 
+const DevUserKey ContextString = "dev-user"
+
 func AuthMiddleware(next http.Handler, clerkKey string) http.Handler {
 	clerk.SetKey(os.Getenv(clerkKey))
 
 	return clerkhttp.WithHeaderAuthorization()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if os.Getenv("ENV") == "dev" {
 			// skip auth in dev mode
-			ctx := context.WithValue(r.Context(), "user", "dev-user")
+			ctx := context.WithValue(r.Context(), DevUserKey, "dev-user")
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
