ac4e9380284e672bca00b19b76db33b4d29dd294
Gnashal <yousifceballos@gmail.com>
2025-10-10 19:41:56 +0800

    fix: optimized price calc routine

--- services/payment/service/price-calc.go
+++ services/payment/service/price-calc.go
@@ -2,7 +2,44 @@ package service
 
 import "handworks/common/grpc/booking"
 
+// hash maps
+var mattressPrices = map[booking.BedType]float32{
+	booking.BedType_KING:           2000.00,
+	booking.BedType_KING_HEADBAND:  2500.00,
+	booking.BedType_QUEEN:          1800.00,
+	booking.BedType_QUEEN_HEADBAND: 2300.00,
+	booking.BedType_DOUBLE:         1500.00,
+	booking.BedType_SINGLE:         1000.00,
+}
+var carPrices = map[booking.CarType]float32{
+	booking.CarType_SEDAN:     3250.00,
+	booking.CarType_MPV:       4000.00,
+	booking.CarType_SUV:       4000.00,
+	booking.CarType_VAN:       5200.00,
+	booking.CarType_PICKUP:    3600.00,
+	booking.CarType_CAR_SMALL: 1750.00,
+}
+
+var couchPrices = map[booking.CouchType]float32{
+	booking.CouchType_SEATER_1:             500.00,
+	booking.CouchType_SEATER_2:             1000.00,
+	booking.CouchType_SEATER_3:             1300.00,
+	booking.CouchType_SEATER_3_LTYPE_SMALL: 1500.00,
+	booking.CouchType_SEATER_3_LTYPE_LARGE: 1750.00,
+	booking.CouchType_SEATER_4_LTYPE_SMALL: 1800.00,
+	booking.CouchType_SEATER_4_LTYPE_LARGE: 2000.00,
+	booking.CouchType_SEATER_5_LTYPE:       2250.00,
+	booking.CouchType_SEATER_6_LTYPE:       2500.00,
+	booking.CouchType_OTTOMAN:              500.00,
+	booking.CouchType_LAZBOY:               900.00,
+	booking.CouchType_CHAIR:                250.00,
+}
+
 func CalculateGeneralCleaning(details *booking.GeneralCleaningDetails) float32 {
+	if details == nil {
+		return 0.0
+	}
+
 	sqm := details.Sqm
 	homeType := details.HomeType
 
@@ -24,29 +61,11 @@ func CalculateCarCleaning(details *booking.CarCleaningDetails) float32 {
 	}
 
 	var total float32
-
 	for _, spec := range details.CleaningSpecs {
-		var pricePerCar float32
-
-		switch spec.CarType {
-		case booking.CarType_SEDAN:
-			pricePerCar = 3250.00
-		case booking.CarType_MPV:
-			pricePerCar = 4000.00
-		case booking.CarType_SUV:
-			pricePerCar = 4000.00
-		case booking.CarType_VAN:
-			pricePerCar = 5200.00
-		case booking.CarType_PICKUP:
-			pricePerCar = 3600.00
-		case booking.CarType_CAR_SMALL:
-			pricePerCar = 1750.00
-		default:
-			pricePerCar = 0.00
-		}
-
-		total += pricePerCar * float32(spec.Quantity)
+		price := carPrices[spec.CarType]
+		total += price * float32(spec.Quantity)
 	}
+
 	if details.ChildSeats > 0 {
 		total += float32(details.ChildSeats) * 250.00
 	}
@@ -60,41 +79,11 @@ func CalculateCouchCleaning(details *booking.CouchCleaningDetails) float32 {
 	}
 
 	var total float32
-
 	for _, spec := range details.CleaningSpecs {
-		var pricePerCouch float32
-
-		switch spec.CouchType {
-		case booking.CouchType_SEATER_1:
-			pricePerCouch = 500.00
-		case booking.CouchType_SEATER_2:
-			pricePerCouch = 1000.00
-		case booking.CouchType_SEATER_3:
-			pricePerCouch = 1300.00
-		case booking.CouchType_SEATER_3_LTYPE_SMALL:
-			pricePerCouch = 1500.00
-		case booking.CouchType_SEATER_3_LTYPE_LARGE:
-			pricePerCouch = 1750.00
-		case booking.CouchType_SEATER_4_LTYPE_SMALL:
-			pricePerCouch = 1800.00
-		case booking.CouchType_SEATER_4_LTYPE_LARGE:
-			pricePerCouch = 2000.00
-		case booking.CouchType_SEATER_5_LTYPE:
-			pricePerCouch = 2250.00
-		case booking.CouchType_SEATER_6_LTYPE:
-			pricePerCouch = 2500.00
-		case booking.CouchType_OTTOMAN:
-			pricePerCouch = 500.00
-		case booking.CouchType_LAZBOY:
-			pricePerCouch = 900.00
-		case booking.CouchType_CHAIR:
-			pricePerCouch = 250.00
-		default:
-			pricePerCouch = 0.00
-		}
-
-		total += pricePerCouch * float32(spec.Quantity)
+		price := couchPrices[spec.CouchType]
+		total += price * float32(spec.Quantity)
 	}
+
 	if details.BedPillows > 0 {
 		total += float32(details.BedPillows) * 100.00
 	}
@@ -103,31 +92,13 @@ func CalculateCouchCleaning(details *booking.CouchCleaningDetails) float32 {
 }
 
 func CalculateMattressCleaning(details *booking.MattressCleaningDetails) float32 {
-	if details == nil || len(details.CleaningSpecs) == 0 {
+	if details == nil {
 		return 0.0
 	}
 
 	var total float32
 	for _, spec := range details.CleaningSpecs {
-		var price float32
-
-		switch spec.BedType {
-		case booking.BedType_KING:
-			price = 2000.00
-		case booking.BedType_KING_HEADBAND:
-			price = 2500.00
-		case booking.BedType_QUEEN:
-			price = 1800.00
-		case booking.BedType_QUEEN_HEADBAND:
-			price = 2300.00
-		case booking.BedType_DOUBLE:
-			price = 1500.00
-		case booking.BedType_SINGLE:
-			price = 1000.00
-		default:
-			price = 0.00
-		}
-
+		price := mattressPrices[spec.BedType]
 		total += price * float32(spec.Quantity)
 	}
 	return total
@@ -137,8 +108,7 @@ func CalculatePostConstructionCleaning(details *booking.PostConstructionCleaning
 	if details == nil {
 		return 0.0
 	}
-	sqm := details.Sqm
-	return float32(sqm * 50.00)
+	return float32(details.Sqm * 50.00)
 }
 
 func (b *PaymentService) CalculatePriceByServiceType(service *booking.ServicesRequest) float32 {
